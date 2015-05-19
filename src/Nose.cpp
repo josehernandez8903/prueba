bffb5b0080f95bc2030b073fa178804827c13542
El nombre que quieran <correo@servidor.mx>
2015-05-19 17:28:33 -0500

    Ultimo commit

--- src/Nose.cpp
+++ src/Nose.cpp
@@ -12,6 +12,5 @@ using namespace std;
 int main() {
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-
 	return 0;
 }
